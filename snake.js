f5baac433390bc35a54a28cff2ac7092e523dd00
leogirardeau <leogir.pro@gmail.com>
2020-09-26 18:06:40 +0200

    gameover and reset game

--- snake.js
+++ snake.js
@@ -1,6 +1,6 @@
 import { getInputDirection } from "./input.js";
 
-export const SNAKE_SPEED = 1;
+export const SNAKE_SPEED = 3;
 const snakeBody = [{ x: 11, y: 11 }];
 let newSegments = 0;
 
@@ -29,10 +29,19 @@ export function expandSnake(amount) {
   newSegments += amount;
 }
 
-export function onSnake(position) {
-  return snakeBody.some((segment) => {
+export function onSnake(position, { ingoreHead = false } = {}) {
+  return snakeBody.some((segment, index) => {
+    if (ingoreHead && index === 0) return false;
     return equalPositions(segment, position);
   });
+}
+
+export function getSnakeHead() {
+  return snakeBody[0];
+}
+
+export function snakeIntersection() {
+  return onSnake(snakeBody[0], { ingoreHead: true });
 }
 
 function equalPositions(pos1, pos2) {
