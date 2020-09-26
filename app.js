f674ea8ce1d748157a24d2b01ef7fba310fde540
leogirardeau <leogir.pro@gmail.com>
2020-09-26 10:38:18 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
+
+let lastRenderTime = 0;
+
+function main(currentTime) {
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+function update() {
+  updateSnake();
+}
+
+function draw() {
+  drawSnake();
+}
---
+++ snake.js
@@ -0,0 +1,5 @@
+export const SNAKE_SPEED = 1;
+
+export function update() {}
+
+export function draw() {}
